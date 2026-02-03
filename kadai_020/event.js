200d469ec59cd3926fbea602769284b391644c8f
wa0102 <minon.maro@gmail.com>
2026-02-03 10:12:30 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn');
+const textT = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+  textT.textContent = 'ボタンをクリックしました';
+  console.log(text);
+});
